18b853f63f6db4b29d5c23e8221e7a8cf00e92d7
Austin Howard <cnfishead@hotmail.com>
2025-05-05 16:08:41 -0400

    trying something

--- Modules/DbModule.cs
+++ Modules/DbModule.cs
@@ -5,15 +5,13 @@ public class DatabaseModule : IBaseModule
 {
   public void RegisterServices(IServiceCollection services)
   {
-    var serviceProvider = services.BuildServiceProvider();
-    var configuration = serviceProvider.GetRequiredService<IConfiguration>();
-
-    string connectionString = configuration.GetConnectionString("DefaultConnection");
+    var config = services.BuildServiceProvider().GetRequiredService<IConfiguration>();
+    var connectionString = config.GetConnectionString("DefaultConnection");
     Console.WriteLine($"Using DB Connection String: {connectionString}"); // Debugging
 
     services.AddDbContext<BotDbContext>(options =>
         options.UseNpgsql(connectionString)
-    );  
+    );
     // ✅ Enable dynamic JSON reading
     NpgsqlConnection.GlobalTypeMapper.EnableDynamicJson();
   }
